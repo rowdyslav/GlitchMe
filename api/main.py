f4d8d93e921148b2cf13dc6fb366f9b90f264534
rowdyslav <rowdyslav@gmail.com>
2025-02-04 20:25:18 +0300

    typing

--- api/main.py
+++ api/main.py
@@ -19,7 +19,7 @@ async def handler(_: Request, exc: ValidationError):
         reformatted_message[".".join(filtered_loc)].append(msg)
 
     return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder(
             {"detail": "Bad request", "errors": reformatted_message}
         ),
