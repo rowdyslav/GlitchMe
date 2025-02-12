53228da8623904801fb5fa990b4e91896876a853
NikolaZhal <ryazan62nik@yandex.ru>
2025-02-12 18:56:26 +0300

    true connect player + webhook(getLINK)

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,8 @@ from environs import Env
 from fastapi import FastAPI
 from icecream import ic
 from uvicorn import Config, Server
+from misc.link import generate_game_link
+from beanie import PydanticObjectId
 
 env = Env()
 env.read_env()
@@ -20,6 +22,16 @@ async def send_message(chat_id: str, text: str):
 
     try:
         await bot.send_message(chat_id=chat_id, text=text)
+    except Exception as e:
+        ic(e)
+
+
+@webhook.get("/get-link/")
+async def get_link(game_id: PydanticObjectId):
+    """отправляет ссылку для подключения к игре"""
+
+    try:
+        return await generate_game_link(bot, game_id)
     except Exception as e:
         ic(e)
 
